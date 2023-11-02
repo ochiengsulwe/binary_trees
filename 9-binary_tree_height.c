df55b23b436e32dd0285d508c0d5b655998806b9
ochiengsulwe <kodongochieng@gmail.com>
2023-11-02 15:16:49 -0700

    first file push

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -10,7 +10,7 @@
 size_t binary_tree_height(const binary_tree_t *tree)
 {
 	size_t l, r;
-	
+
 	if (tree)
 	{
 		l = 0, r = 0;
--- binary_trees.h
+++ binary_trees.h
@@ -30,12 +30,8 @@ typedef struct binary_tree_s bst_t;
 typedef struct binary_tree_s avl_t;
 typedef struct binary_tree_s heap_t;
 
-/* binary_tree_print.c */
-void binary_tree_print(const binary_tree_t *);
-
-/* tasks */
+void binary_tree_print(const binary_tre
 binary_tree_t *binary_tree_node(binary_tree_t *parent, int value);
-binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value);
 binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value);
 void binary_tree_delete(binary_tree_t *tree);
 int binary_tree_is_leaf(const binary_tree_t *node);
